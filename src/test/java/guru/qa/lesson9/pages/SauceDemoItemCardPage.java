87bd9f3680e420b761d3714201a46ef8cc5c64f7
Dmitrii <d.kaliuzhyni@fhl.world>
2024-11-12 22:34:49 +0300

    correct check method to void

--- src/test/java/guru/qa/lesson9/pages/SauceDemoItemCardPage.java
+++ src/test/java/guru/qa/lesson9/pages/SauceDemoItemCardPage.java
@@ -8,8 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class SauceDemoItemCardPage {
     private final SelenideElement basketButton = $(".inventory_details_name");
 
-    public SauceDemoItemCardPage checkItemNameIsCorrect(String name) {
+    public void checkItemNameIsCorrect(String name) {
         basketButton.shouldHave(text(name));
-        return this;
     }
 }
