00f629dca7d5e8a4833f9653b941605777afdae5
Dmitrii <d.kaliuzhyni@fhl.world>
2024-11-19 17:29:45 +0300

    add test and task for a jenkins console

---
+++ src/test/java/guru/qa/lesson12/jenkins/RegistrationPageTests.java
@@ -0,0 +1,42 @@
+package guru.qa.lesson12.jenkins;
+
+
+import guru.qa.lesson12.jenkins.pages.RegistrationPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("remoteControlJenkins")
+public class RegistrationPageTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fillWholeDataFormWithAllureAndJenkinsTest() {
+        registrationPage.openPage()
+                .setFirstName("Arthas")
+                .setLastName("Menethil")
+                .setEmail("lichking@blizzard.com")
+                .setGender("Male")
+                .setUserNumber("9990009343")
+                .setDateOfBirth("01", "January", "1990")
+                .setSubjectsInput("Maths")
+                .setHobbiesWrapper("Music")
+                .uploadPicture("guru_png.png")
+                .setCurrentAddress("Frozen Throne")
+                .setState("Uttar Pradesh")
+                .setCity("Agra")
+                .submit();
+
+        registrationPage
+                .checkTableResponse("Student Name", "Arthas Menethil")
+                .checkTableResponse("Student Email", "lichking@blizzard.com")
+                .checkTableResponse("Gender", "Male")
+                .checkTableResponse("Mobile", "9990009343")
+                .checkTableResponse("Date of Birth", "01 January,1990")
+                .checkTableResponse("Subjects", "Maths")
+                .checkTableResponse("Hobbies", "Music")
+                .checkTableResponse("Picture", "guru_png.png")
+                .checkTableResponse("Address", "Frozen Throne")
+                .checkTableResponse("State and City", "Uttar Pradesh Agra");
+    }
+}
