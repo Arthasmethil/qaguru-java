ec8c4fd2af6071fe5aae52af4faa9ed506e22acb
Dmitrii <d.kaliuzhyni@fhl.world>
2024-11-21 00:08:07 +0300

    configure TestBase for jenkinfs

--- src/test/java/guru/qa/lesson12/jenkins/TestBase.java
+++ src/test/java/guru/qa/lesson12/jenkins/TestBase.java
@@ -6,6 +6,9 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static guru.qa.lesson12.jenkins.helpers.Attach.*;
 
@@ -15,8 +18,20 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("remoteUrl");
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
         // for debug purpose only
 //        Configuration.holdBrowserOpen = true;
     }
