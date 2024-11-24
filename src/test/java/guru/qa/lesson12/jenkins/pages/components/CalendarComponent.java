476246b9467aa9203ecffb3a56a1e3a8741c11b0
Dmitrii <d.kaliuzhyni@fhl.world>
2024-11-24 22:22:39 +0300

    fix date for test data

--- src/test/java/guru/qa/lesson12/jenkins/pages/components/CalendarComponent.java
+++ src/test/java/guru/qa/lesson12/jenkins/pages/components/CalendarComponent.java
@@ -4,6 +4,9 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
+        if (Integer.parseInt(day) < 10) {
+            day = "0" + day;
+        }
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
