e9b7e20f9396cc1f6a27600757e2cbd49e5874be
Dmitrii <d.kaliuzhyni@fhl.world>
2025-01-16 22:15:40 +0300

    fix priorities of auth config

--- src/test/java/guru/qa/lesson18/config/AuthConfig.java
+++ src/test/java/guru/qa/lesson18/config/AuthConfig.java
@@ -2,6 +2,8 @@ package guru.qa.lesson18.config;
 
 import org.aeonbits.owner.Config;
 
+import java.net.URL;
+
 @Config.Sources({
         "file:tml/auth.properties",
         "classpath:auth.properties",
@@ -14,4 +16,7 @@ public interface AuthConfig extends Config {
 
     @Key("password")
     String password();
+
+    @Key("remoteHost")
+    URL remoteHost();
 }
