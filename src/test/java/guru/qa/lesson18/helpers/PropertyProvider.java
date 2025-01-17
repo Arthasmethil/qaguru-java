2a3f737ec8398bd30723c4709612d88fee9b008a
Dmitrii <d.kaliuzhyni@fhl.world>
2025-01-17 19:46:07 +0300

    jenkins debug

--- src/test/java/guru/qa/lesson18/helpers/PropertyProvider.java
+++ src/test/java/guru/qa/lesson18/helpers/PropertyProvider.java
@@ -9,22 +9,21 @@ public class PropertyProvider {
 
     static {
         try (InputStream input = PropertyProvider.class.getClassLoader().getResourceAsStream("auth.properties")) {
-            if (input == null) {
-                throw new RuntimeException("auth.properties not found");
+            if (input != null) {
+                properties.load(input);
+            } else {
+                System.out.println("Warning: auth.properties not found, using system properties only");
             }
-            properties.load(input);
         } catch (IOException e) {
             throw new RuntimeException("Failed to load properties file", e);
         }
     }
 
     public static String getProperty(String key) {
-        if (System.getProperty(key) != null) {
-            return System.getProperty(key);
+        String value = System.getProperty(key, properties.getProperty(key));
+        if (value == null) {
+            throw new IllegalArgumentException("Property '" + key + "' is not set");
         }
-        if (properties.getProperty(key) != null) {
-            return properties.getProperty(key);
-        }
-        throw new IllegalArgumentException("Property '" + key + "' is not set");
+        return value;
     }
 }
